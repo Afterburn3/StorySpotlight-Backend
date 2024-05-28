199984a5dd5f1c9f88dbe587ea358fdae52f15e8
kimsengthai <kimseng.thai@hotmail.com>
2024-05-29 03:11:32 +1000

    bacl

--- db/index.js
+++ db/index.js
@@ -7,9 +7,6 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 export const query = (text, params) => {
