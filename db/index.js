8ea89703318d36bd6f536293525d85c027dc7f71
kimsengthai <kimseng.thai@hotmail.com>
2024-05-29 02:01:26 +1000

    database

--- db/index.js
+++ db/index.js
@@ -1,8 +1,12 @@
 import pg from "pg";
 const { Pool } = pg;
 
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
 
-export const query = (text, params, callback) => {
-  return pool.query(text, params, callback);
-};
+export default pool;
